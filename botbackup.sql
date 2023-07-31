1f561002e0ac0f9a3f8db34c866ac8fdb9c3dce9
! Ruwin.zkr <110337782+Ruwin-dsc@users.noreply.github.com>
2023-07-31 17:30:12 +0200

    Add files via upload

---
+++ botbackup.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : lun. 31 juil. 2023 à 17:29
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `botbackup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `backup`
+--
+
+CREATE TABLE `backup` (
+  `name` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `backup`
+--
+
+INSERT INTO `backup` (`name`, `link`) VALUES
+('betterbots', 'https://discord.com/template/8pkHmGcg7Rf4');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `banner`
+--
+
+CREATE TABLE `banner` (
+  `name` varchar(255) DEFAULT NULL,
+  `link` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
